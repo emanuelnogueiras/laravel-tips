68f48092d0652e2f0971434b6ab0f59d0711a1fb
Emanuel Nogueiras <emanuelnogueiras@gmail.com>
2020-03-22 10:05:25 +0100

    buscador autocompletar

--- resources/views/livewire/buscador-usuarios.blade.php
+++ resources/views/livewire/buscador-usuarios.blade.php
@@ -2,19 +2,42 @@
     <div class="row">
         <div class="col-md-8">
             <div class="form-group">
-                <label for="buscar"><strong>Buscar</strong></label>
-                <input wire:model="buscar" type="text" class="form-control" id="buscar">
-                @error("buscar")
-                <small class="form-text text-danger">{{$message}}</small>
+                <label for="buscar">
+                    <strong>Buscar</strong>
+                    @if($picked)
+                        <span class="badge badge-success">Picked</span>
+                    @else
+                        <span class="badge badge-danger">Picked</span>
+                    @endif
+                </label>
+                <input wire:model="buscar" 
+                    wire:keydown.enter="asignarPrimero()" type="text" class="form-control" id="buscar">
+                @error("buscar")                    
+                    <small class="form-text text-danger">{{$message}}</small>                                    
                 @else
-                    <small class="form-text text-muted">Comienza a teclear para que aparezcan los resultados</small>
+                    @if(count($usuarios)>0)
+                        @if(!$picked)
+                        <div class="shadow rounded px-3 pt-3 pb-0">
+                            @foreach($usuarios as $usuario)
+                                <div style="cursor: pointer;">
+                                    <a wire:click="asignarUsuario('{{ $usuario->name }}')">
+                                        {{ $usuario->name }}
+                                    </a>
+                                </div>
+                                <hr>
+                            @endforeach
+                        </div>
+                        @endif
+                    @else
+                        <small class="form-text text-muted">Comienza a teclear para que aparezcan los resultados</small>
+                    @endif
                 @enderror
             </div>
         </div>
         <div class="col-md-4">
             <p><strong>Resultados</strong></p>
             <p>
-            @foreach($usuarios ?? ["uno", "dos"] as $usuario)
+            @foreach($usuarios as $usuario)
                 <span class="badge badge-secondary">{{ $usuario->name }}</span>
             @endforeach
             </p>
